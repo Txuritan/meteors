f44da1e677b385b3e73e319491fd3871a3570f90
Txuritan <txuritan@protonmail.com>
2021-03-30 23:01:13 -0400

    Very tiny frontend change

--- src/router.rs
+++ src/router.rs
@@ -11,13 +11,6 @@ pub static PAGE_404: &str = r#"<!DOCTYPE html><html><head><title>404 | local arc
 pub static PAGE_503: &str = r#"<!DOCTYPE html><html><head><title>503 | local archive</title><style>*{transition:all .6s}html{height:100%}body{font-family:sans-serif;color:#888;margin:0}#main{display:table;width:100%;height:100vh;text-align:center}.fof{display:table-cell;vertical-align:middle}.fof h1{font-size:50px;display:inline-block;padding-right:12px;animation:type .5s alternate infinite}@keyframes type{from{box-shadow:inset -3px 0 0 #888}to{box-shadow:inset -3px 0 0 transparent}}</style></head><body><div id="main"><div class="fof"><h1>Error 503</h1></div></div></body></html>"#;
 
 macro_rules! res {
-    (400) => {
-        Response::from_string(PAGE_400)
-            .with_header(
-                Header::from_bytes(&b"Content-Type"[..], &b"text/html; charset=utf-8"[..]).unwrap(),
-            )
-            .with_status_code(400)
-    };
     (404) => {
         Response::from_string(PAGE_404)
             .with_header(
