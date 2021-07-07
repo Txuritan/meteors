935581b9b9965c86a466b7fe503abcfee5acbe55
Txuritan <txuritan@protonmail.com>
2021-07-06 20:38:19 -0400

    Going to use TailwindCSS instead of a custom framework

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: [
+        './meteors/command/serve/templates/**/*.hbs',
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
