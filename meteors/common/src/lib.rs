d565eed023fcaf7b648a87737fdc91f4df31afe9
Txuritan <txuritan@protonmail.com>
2021-08-05 23:06:47 -0400

    Remove unused dependencies

--- meteors/common/src/lib.rs
+++ meteors/common/src/lib.rs
@@ -8,9 +8,7 @@ pub type Args = std::iter::Peekable<std::iter::Skip<std::env::Args>>;
 
 pub mod prelude {
     pub use {
-        crate::utils::new_id,
         ::anyhow::{self, anyhow, bail, Context as _, Result},
-        either::{Left, Right},
         log::{debug, error, info, trace, warn},
         owo_colors::OwoColorize as _,
     };
