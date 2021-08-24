518d3efd9411cc5bb45ead1800ba18b182e910ee
Txuritan <txuritan@protonmail.com>
2021-08-24 15:51:09 -0400

    Switch to AFL for fuzzing, added some seeds as well
    Got it all working in WSL so now I can actually run it

--- fuzz/fuzz_targets/http_parse.rs
+++ fuzz/fuzz_targets/http_parse.rs
@@ -1,6 +1,5 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    let _ = enrgy::http::HttpRequest::fuzz_parse_reader(&mut std::io::Cursor::new(data));
-});
+fn main() {
+    afl::fuzz!(|data: &[u8]| {
+        let _ = enrgy::http::HttpRequest::fuzz_parse_reader(&mut std::io::Cursor::new(data));
+    });
+}
